4492bda1e30b31bb7b55a6582850792c12645cbb
shien30 <shienloh504@gmail.com>
2026-03-10 15:04:29 +0100

    feat:add student struct and Grade Enum

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 fn main() {
     use std::io::{self, Write};
 
+// 1. Grade Enum
+// We use #[derive(Debug)] so we can easily print the enum variants using {:?}
+#[derive(Debug)]
+enum Grade {
+    Excellent,
+    VeryGood,
+    Good,
+    Pass,
+    Fail,
+}
+
+// 2. Student Struct
+struct Student {
+    id: u32,
+    name: String,   // String owns its data (unlike &str which borrows)
+    age: u32,
+    scores: Vec<u32>, // A dynamic, growable list of numbers
+}
+
    
